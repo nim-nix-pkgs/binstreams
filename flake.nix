84ce4e35f12140035b9a5425090a247abf957b9d
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:36 -0300

    chore: re index binstreams

--- flake.nix
+++ flake.nix
@@ -7,38 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."binstreams-master".dir   = "master";
-  inputs."binstreams-master".owner = "nim-nix-pkgs";
-  inputs."binstreams-master".ref   = "master";
-  inputs."binstreams-master".repo  = "binstreams";
-  inputs."binstreams-master".type  = "github";
-  inputs."binstreams-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."binstreams-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."binstreams-v0_1_0".dir   = "v0_1_0";
-  inputs."binstreams-v0_1_0".owner = "nim-nix-pkgs";
-  inputs."binstreams-v0_1_0".ref   = "master";
-  inputs."binstreams-v0_1_0".repo  = "binstreams";
-  inputs."binstreams-v0_1_0".type  = "github";
-  inputs."binstreams-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."binstreams-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."binstreams-v0_1_1".dir   = "v0_1_1";
-  inputs."binstreams-v0_1_1".owner = "nim-nix-pkgs";
-  inputs."binstreams-v0_1_1".ref   = "master";
-  inputs."binstreams-v0_1_1".repo  = "binstreams";
-  inputs."binstreams-v0_1_1".type  = "github";
-  inputs."binstreams-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."binstreams-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."binstreams-v0_2_0".dir   = "v0_2_0";
-  inputs."binstreams-v0_2_0".owner = "nim-nix-pkgs";
-  inputs."binstreams-v0_2_0".ref   = "master";
-  inputs."binstreams-v0_2_0".repo  = "binstreams";
-  inputs."binstreams-v0_2_0".type  = "github";
-  inputs."binstreams-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."binstreams-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
